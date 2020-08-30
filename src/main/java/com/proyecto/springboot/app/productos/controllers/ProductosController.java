3ad2ac538ac5fb8b1fafb0951ff4e04c37683ff0
juan <juan.antoniorr@hotmail.com>
2020-08-30 15:09:39 -0500

    Adding CRUD

--- src/main/java/com/proyecto/springboot/app/productos/controllers/ProductosController.java
+++ src/main/java/com/proyecto/springboot/app/productos/controllers/ProductosController.java
@@ -1,13 +1,20 @@
 package com.proyecto.springboot.app.productos.controllers;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.proyecto.springboot.app.productos.models.entity.Producto;
@@ -45,5 +52,28 @@ public class ProductosController {
 			e.printStackTrace();
 		} */
 		return producto;
+	}
+	@PostMapping ("/crear")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Producto guardar(@RequestBody Producto producto) { 
+		//con la anotacion request body indicamos que el producto vendra dentro del body del json
+		return productoService.save(producto);
+		
+	}
+	@DeleteMapping ("/eliminar/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public void eliminar (@PathVariable Long id) {
+		productoService.deleteById(id);
+	}
+	@PutMapping ("/editar/{id}")
+	@ResponseStatus (HttpStatus.CREATED)
+	public Producto actualizar(@RequestBody Producto producto,@PathVariable Long id) {
+		Producto productoDB = productoService.findById(id);
+		productoDB.setNombre(producto.getNombre());
+		productoDB.setPrecio(producto.getPrecio());
+		productoDB.setCreateAt(new Date());
+		return productoService.save(productoDB);
+		
+		
 	}
 }
--- src/main/java/com/proyecto/springboot/app/productos/models/service/ProductoServiceImpl.java
+++ src/main/java/com/proyecto/springboot/app/productos/models/service/ProductoServiceImpl.java
@@ -1,7 +1,6 @@
 package com.proyecto.springboot.app.productos.models.service;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,6 +24,20 @@ public class ProductoServiceImpl implements IProductoService {
 	public Producto findById(Long id) {
 		
 		return productoDao.findById(id).orElse(null);
+	}
+
+	@Override
+	@Transactional
+	public Producto save(Producto producto) {
+		
+		return productoDao.save(producto);
+	}
+
+	@Override
+	@Transactional
+	public void deleteById(Long id) {
+		 productoDao.deleteById(id);
+		
 	}
 
 }
